707a5d87fadf397df393fa01fa1c980b9b4d2aba
Maurizio Crocci <mbcrocci@gmail.com>
2017-12-04 14:51:26 +0000

    Projeto criado

---
+++ app/src/main/java/pt/isec/gps1718_g15/memberme/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.isec.gps1718_g15.memberme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
