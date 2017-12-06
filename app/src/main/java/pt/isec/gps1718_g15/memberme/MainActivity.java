731b2b81d1f41485b7e57fc95a2e81b5ded615c6
Pedro Oliveira <p5489pmdo@hotmail.com>
2017-12-06 19:05:17 +0000

    botao next

--- app/src/main/java/pt/isec/gps1718_g15/memberme/MainActivity.java
+++ app/src/main/java/pt/isec/gps1718_g15/memberme/MainActivity.java
@@ -2,6 +2,7 @@ package pt.isec.gps1718_g15.memberme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_bar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
